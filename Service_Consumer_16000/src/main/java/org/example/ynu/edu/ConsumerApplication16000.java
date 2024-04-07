d529a7c819b09dfd8187bd42180eb865e2c1e59b
watermelonFish <1399796835@qq.com>
2024-04-07 22:00:05 +0800

    完成负载均衡策略：随机+轮询n次

--- Service_Consumer_16000/src/main/java/org/example/ynu/edu/ConsumerApplication16000.java
+++ Service_Consumer_16000/src/main/java/org/example/ynu/edu/ConsumerApplication16000.java
@@ -1,15 +1,15 @@
 package org.example.ynu.edu;
 
+import org.example.ynu.edu.rule.CustomRandomLoadBalancerConf;
+import org.example.ynu.edu.rule.CustomThreeTimeLoadBalancerConf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
-@LoadBalancerClient(name = "provider-server")
+@LoadBalancerClient(name = "provider-server", configuration = CustomThreeTimeLoadBalancerConf.class)
 public class ConsumerApplication16000 {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication16000.class,args);

6c357f030f3feb25b30deddc3c2e24ea52edfa61
watermelonFish <1399796835@qq.com>
2024-04-07 19:59:27 +0800

    完成ResTemplate-get/post/put/delete服务之间调用（由于RestemPlate所提供的方法的局限性.getForObject()/.postForObject()/.put()/.delete()

--- Service_Consumer_16001/src/main/java/org/example/ynu/edu/controller/CartController.java
+++ Service_Consumer_16001/src/main/java/org/example/ynu/edu/controller/CartController.java
@@ -1,13 +1,17 @@
 package org.example.ynu.edu.controller;
 
 import jakarta.annotation.Resource;
-import org.example.ynu.edu.entity.CommonResult;
-import org.example.ynu.edu.entity.User;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.example.ynu.edu.entity.CommonResult;
+import org.example.ynu.edu.entity.User;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cart")
@@ -17,10 +21,19 @@ public class CartController {
     @Resource
     private RestTemplate restTemplate;
 
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/getCartById/{userId}")
     public CommonResult<User> getCartById(@PathVariable("userId") Integer userId){
+        //通过服务提供者名（provider-server）获取Eureka server上的服务注册列表
+        List<ServiceInstance> instanceList = discoveryClient.getInstances("provider-server");//这你就写这个controller想要调用的服务的名字
+        for(ServiceInstance si : instanceList){
+            System.out.println(si.getHost()+"\t"+si.getPort());
+        }
+        ServiceInstance instance = instanceList.get(0);
         CommonResult<User> result = restTemplate.getForObject(
-                "http://localhost:11001/user/getUserById/"+userId.toString(),//这里的url需要写完整
+                "http://"+instance.getHost()+":"+instance.getPort()+"/user/getUserById/"+userId.toString(),
                 CommonResult.class
         );
         return result;
--- Service_Provider_11000/src/main/java/org/example/ynu/edu/controller/UserController.java
+++ Service_Provider_11000/src/main/java/org/example/ynu/edu/controller/UserController.java
@@ -2,10 +2,7 @@ package org.example.ynu.edu.controller;
 
 import org.example.ynu.edu.entity.CommonResult;
 import org.example.ynu.edu.entity.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -17,6 +14,85 @@ public class UserController {
         String message = "success(11000)";
         try{
             User u = new User(userId, "小明", "123456");
+            result.setResult(u);
+        }
+        catch(Exception e){
+            code = 500;
+            message = "failed";
+        }
+        result.setMessage(message);
+        result.setCode(code);
+        System.out.println(11000);
+        return result;
+    }
+
+    @PostMapping("/saveUserByRequestBody")
+    public CommonResult<User> saveUserByRequestBody(@RequestBody User user){
+        //ResquestBody需要发送json格式
+        CommonResult<User> result = new CommonResult<>();
+        Integer code = 200;
+        String message = "success(11000)";
+        try{
+            result.setResult(user);
+        }
+        catch(Exception e){
+            code = 500;
+            message = "failed";
+        }
+        result.setMessage(message);
+        result.setCode(code);
+        System.out.println(11000);
+        return result;
+    }
+
+
+    @PostMapping("/saveUserByRequestParam")
+    public CommonResult<User> saveUserByRequestParam(@RequestParam("userId") Integer userId){
+        CommonResult<User> result = new CommonResult<>();
+        Integer code = 200;
+        String message = "success(11000)";
+        try{
+            User u = new User(userId, "小明", "123456");
+            result.setResult(u);
+        }
+        catch(Exception e){
+            code = 500;
+            message = "failed";
+        }
+        result.setMessage(message);
+        result.setCode(code);
+        System.out.println(11000);
+        return result;
+    }
+
+    @PutMapping("/putUser")
+    public CommonResult<User> putUser(@RequestBody User user){
+        //ResquestBody需要发送json格式
+        CommonResult<User> result = new CommonResult<>();
+        Integer code = 200;
+        String message = "success(11000)";
+        try{
+            result.setResult(user);
+        }
+        catch(Exception e){
+            code = 500;
+            message = "failed";
+        }
+        result.setMessage(message);
+        result.setCode(code);
+        System.out.println(11000);
+        return result;
+    }
+
+    @DeleteMapping("/deleteUserById/{userId}")
+    public CommonResult<User> deleteUserById(@PathVariable("userId") Integer userId){
+        //@RequestParam("userId") Integer userId，这个括号里面一定要标明是userId
+        //使用apifox测试时，需要通过param来测试
+        CommonResult<User> result = new CommonResult<>();
+        Integer code = 200;
+        String message = "success(11000)";
+        try{
+            User u = new User(userId, "小明(delete)", "123456");
             result.setResult(u);
         }
         catch(Exception e){
